e99951500c7342d34566a82988dc1dce80f74101
changxin.zhou <changxin.zhou@bst.ai>
2025-03-21 11:14:34 +0800

    debug

--- models/team12_DiffBIR/diffbir/inference/bfr_loop.py
+++ models/team12_DiffBIR/diffbir/inference/bfr_loop.py
@@ -16,7 +16,7 @@ class BFRInferenceLoop(InferenceLoop):
 
     def load_cleaner(self) -> None:
         self.cleaner: SwinIR = instantiate_from_config(
-            OmegaConf.load(args.train_cfg)
+            OmegaConf.load(self.args.train_cfg)
         )
         # weight = load_model_from_url(MODELS["swinir_face"])
         weight = load_model_from_path(args.swinir_path)
